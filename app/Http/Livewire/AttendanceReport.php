612ad81770c7f52c05d12aac32eaf915e52e80d9
Ella-May <20191267@cityofmalabonuniversity.edu.ph>
2023-04-06 19:40:05 +0800

    first commit

---
+++ app/Http/Livewire/AttendanceReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AttendanceReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.attendance-report');
+    }
+}
